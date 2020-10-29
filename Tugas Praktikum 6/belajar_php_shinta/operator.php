bf4a6f878ab192975c817c67c366172d988ce9db
Shinta0212 <destiera02@gmail.com>
2020-10-29 10:07:09 +0700

    Mengumpulkan Tugas (Shinta)

---
+++ Tugas Praktikum 6/belajar_php_shinta/operator.php
@@ -0,0 +1,36 @@
+<?php
+
+$a = 5;
+$b = 2;
+
+// penjumlahan
+$c = $a + $b;
+echo "$a + $b = $c";
+echo "<hr>";
+
+// pengurangan
+$c = $a - $b;
+echo "$a - $b = $c";
+echo "<hr>";
+
+// perkalian
+$c = $a * $b;
+echo "$a * $b = $c";
+echo "<hr>";
+
+// pembagian
+$c = $a / $b;
+echo "$a / $b = $c";
+echo "<hr>";
+
+// Sisa bagi
+$c = $a % $b;
+echo "$a % $b = $c";
+echo "<hr>";
+
+// Pangkat
+$c = $a ** $b;
+echo "$a ** $b = $c";
+echo "<hr>";
+
+?>
---
+++ Tugas Praktikum 6/belajar_php_shinta/print.php
@@ -0,0 +1,5 @@
+<?php
+print "<h2>Belajar PHP dari No1!<h2>";
+print "Hello World !<br>";
+print "Belajar Mencetak Teks di PHP!";
+?>
