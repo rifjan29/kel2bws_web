0a2180e75e3974ecd62e267be081d6aedd016a70
Auliaaqsonia <72010264+Auliaaqsonia@users.noreply.github.com>
2020-12-24 20:58:44 +0700

    Delete delete-data.php

--- SMPN7Bondowoso/admin/public/cms/profile_pimpinan/delete-data.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'C:\xampp\htdocs\SMPN7Bondowoso\admin\config\conn.php';
-$id = $_GET['id'];
-mysqli_query($db, "DELETE FROM headmaster WHERE id = '$id'");
-
-header("location:index.php?pesan=input");
-?>
